0aa541aa2830c94770099901608dfc596b9970ad
hernan <hermerino2@gmail.com>
2023-08-30 08:37:24 -0300

    Actualizacion de archivos

---
+++ app/src/androidTest/java/com/curso/android/app/practica/appintegradora/MainActivityTest.kt
@@ -0,0 +1,202 @@
+package com.curso.android.app.practica.appintegradora
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.*
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    @Rule
+    @JvmField
+    var mActivityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun mainActivityTest() {
+
+        Thread.sleep(500)
+
+        val appCompatEditText = onView(
+            allOf(
+                withId(R.id.text1),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    2
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText.perform(click())
+
+        val appCompatEditText2 = onView(
+            allOf(
+                withId(R.id.text1),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    2
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText2.perform(replaceText("ASD"), closeSoftKeyboard())
+
+        val appCompatEditText3 = onView(
+            allOf(
+                withId(R.id.text2),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    3
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText3.perform(replaceText("ASD"), closeSoftKeyboard())
+
+        val appCompatEditText4 = onView(
+            allOf(
+                withId(R.id.text2), withText("ASD"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    3
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText4.perform(pressImeActionButton())
+
+        val appCompatButton = onView(
+            allOf(
+                withId(R.id.bot1), withText("Comparar"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatButton.perform(click())
+
+        val appCompatEditText5 = onView(
+            allOf(
+                withId(R.id.text2), withText("ASD"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    3
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText5.perform(click())
+
+        val appCompatEditText6 = onView(
+            allOf(
+                withId(R.id.text2), withText("ASD"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    3
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText6.perform(replaceText("ADS"))
+
+        val appCompatEditText7 = onView(
+            allOf(
+                withId(R.id.text2), withText("ADS"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    3
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText7.perform(closeSoftKeyboard())
+
+        val appCompatEditText8 = onView(
+            allOf(
+                withId(R.id.text2), withText("ADS"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    3
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText8.perform(pressImeActionButton())
+
+        val appCompatButton2 = onView(
+            allOf(
+                withId(R.id.bot1), withText("Comparar"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatButton2.perform(click())
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
---
+++ app/src/test/MainActivityUnitTest.kt
@@ -0,0 +1,41 @@
+
+import androidx.lifecycle.ViewModel
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.curso.android.app.practica.appintegradora.MainActivity
+import kotlinx.coroutines.test.*
+import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.runner.RunWith
+
+annotation class Test
+
+
+
+
+    @RunWith(AndroidJUnit4::class)
+
+    class MainActivityUnitTest {
+
+        private lateinit var viewModel: MainActivity
+
+        @get:Rule
+        val instantTaskRule = InstantTaskExecutorRule()
+
+        private val dispatcher = StandardTestExecutor()
+
+        fun testEditTextIsEmpty() = runTest
+
+
+
+
+    @Test
+
+
+
+
+    }
+    }
+
+
+}
